79522de5ac1a4f57bf7293cc6483a377d47385c5
Julia <julia.dzianisenka@gmail.com>
2022-12-05 15:48:01 +0100

    add Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 
 export function Footer({ author, time, commentsAmount }) {
-
+  return (
+        <div>
+            <span>{author}</span>
+            <span>{moment.unix(time).fromNow()}</span>
+            <span>
+                <button>Comments</button>
+                {commentsAmount}
+            </span>
+        </div>
+  )
 }
