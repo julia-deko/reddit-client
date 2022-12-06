9f80905a980ec03664af47ca9006f4ee5b996a78
Julia <julia.dzianisenka@gmail.com>
2022-12-06 15:43:23 +0100

    add Comment component

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 
 export function Comment({ author, text, time }) {
-
+  return (
+    <div>
+      <div>
+        <p>{author}</p>
+        <p>{moment.unix(time).fromNow()}</p>
+      </div>
+      <p>{text}</p>
+    </div>
+  );
 }
