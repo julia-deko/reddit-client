a724513eb18d9acca322915b85ec0960154c86dc
Julia <julia.dzianisenka@gmail.com>
2022-12-19 16:11:45 +0100

    add styles to Comment component

---
+++ src/components/Comment/CommentStyles.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const CommentWrapper = styled.div`
+  background-color: #fcfcfc;
+  margin: 8px 0;
+  padding: 16px;
+  transition: box-shadow 0.1s ease-in;
+  border-radius: 4px;
+  color: #717171;
+`;
+
+export const Metadata = styled.div`
+  display: flex;
+  margin-bottom: 16px;
+  align-items: center;
+`;
+
+export const Author = styled.p`
+  font-weight: bold;
+  color: #3d5af1;;
+`;
+
+export const Time = styled.p`
+  margin-left: auto;
+  font-style: italic;
+`;
