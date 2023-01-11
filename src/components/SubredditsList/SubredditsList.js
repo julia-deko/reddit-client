0550ef257d3cc2721dfb349729663319afeb5f95
Julia <julia.dzianisenka@gmail.com>
2023-01-11 17:00:44 +0100

    add styles to SubredditsList component

--- src/components/SubredditsList/SubredditsList.js
+++ src/components/SubredditsList/SubredditsList.js
@@ -1,9 +1,10 @@
 import { Subreddit } from '../Subreddit/Subreddit';
+import { Title } from './SubredditsListStyles';
 
 export function SubredditsList({subreddits}) {
   return (
     <div>
-      <h2>Subreddits</h2>
+      <Title>Subreddits</Title>
       <div>
         {subreddits.map(subreddit => {
           return <Subreddit key={subreddit.id} title={subreddit.display_name} imageURL={subreddit.icon_img} />;
---
+++ src/components/SubredditsList/SubredditsListStyles.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { Colors } from '../../common/constants/colors';
+
+export const Title = styled.h2`
+  margin-top: 0;
+  color: ${Colors.header};
+  font-size: 24px;
+`;
